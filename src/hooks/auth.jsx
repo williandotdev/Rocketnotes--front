f02a75ef30321e714501feb4f73322f56ac0028d
Willian <willian.dev.ferreira@gmail.com>
2024-05-26 15:28:16 -0300

    feat: localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import {api} from "../services/api";
 
 export const AuthContext = createContext({});
@@ -10,9 +10,14 @@ function AuthProvider({ children }){
         try{
             const response = await api.post("/sessions", {email, password});
             const {user, token} = response.data;
+
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
+            localStorage.setItem("@rocketnotes:token", token);
+
+
             api.defaults.headers.authorization = `Bearer ${token}`;
 
-            setData({user, token})
+            setData({user, token});
             
         } catch (error){
             if(error.response){
@@ -22,6 +27,22 @@ function AuthProvider({ children }){
             )
         }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user){
+            api.defaults.headers.authorization = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+            })
+        }
+    }, []);
+
+
     return(
         <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
