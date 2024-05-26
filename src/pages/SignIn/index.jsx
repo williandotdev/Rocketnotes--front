3a8b6d6bc41be5431e61f585e4059d009fc99050
Willian <willian.dev.ferreira@gmail.com>
2024-05-26 14:34:05 -0300

    feat: SignIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/auth';
@@ -6,9 +7,14 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from './styles';
 
 export function SignIn() {
-  const data =  useAuth();
-  console.log("Meu contexto =>", data);
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
 
+  const { signIn } =  useAuth();
+
+  function handleSignIn(){
+    signIn({email, password});
+  }
   return (
     <Container>
       <Form>
@@ -21,15 +27,17 @@ export function SignIn() {
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e =>setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e =>setPassword(e.target.value)}
         />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn}/>
 
         <Link to="/register">
           Criar conta
