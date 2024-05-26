26249bfd433121b6d88b193d99d43f87e0d1376a
Willian <willian.dev.ferreira@gmail.com>
2024-05-26 16:43:42 -0300

    refactor: update axios

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -15,7 +15,7 @@ function AuthProvider({ children }){
             localStorage.setItem("@rocketnotes:token", token);
 
 
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common[`Authorization`] = `Bearer ${token}`;
 
             setData({user, token});
             
@@ -40,7 +40,7 @@ function AuthProvider({ children }){
         const user = localStorage.getItem("@rocketnotes:user");
 
         if(token && user){
-            api.defaults.headers.authorization = `Bearer ${token}`;
+            api.defaults.headers.common[`Authorization`] = `Bearer ${token}`;
 
             setData({
                 token,
