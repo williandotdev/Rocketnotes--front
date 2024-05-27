947771246d170f3f839fe784708002e0f6ef06f2
Willian <willian.dev.ferreira@gmail.com>
2024-05-27 15:02:15 -0300

    feat: filter notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,37 @@
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
-
+import { useState, useEffect } from 'react'
 import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
+import { api } from '../../services/api'
 
 export function Home() {
+  const[tags, setTags] = useState([]);
+  const[tagsSelected, setTagsSelected] = useState([]);
+
+  function handleTagSelected(tagName){
+    const alreadySelected = tagsSelected.includes(tagName);
+
+    if(alreadySelected){
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+      setTagsSelected(filteredTags);
+    } else{
+      setTagsSelected(prevState => [...prevState, tagName])      
+    }
+  }
+
+  useEffect(()=>{
+    async function fetchTags(){
+      const response = await api.get("/tags");
+      setTags(response.data);
+
+    }
+
+    fetchTags();
+  }, [])
   return (
     <Container>
       <Brand>
@@ -17,9 +41,24 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+      <li>
+        <ButtonText 
+          title="Todos" 
+          onClick={() => handleTagSelected("todos")}
+          isActive={tagsSelected.length === 0}
+        />
+      </li>
+      {
+        tags && tags.map(tag => (
+          <li key={String(tag.id)}>
+            <ButtonText 
+              title={tag.name} 
+              onClick={() => handleTagSelected(tag.name)}
+              isActive={tagsSelected.includes(tag.name)}
+            />
+          </li>
+        ))
+      }
       </Menu>
 
       <Search>
