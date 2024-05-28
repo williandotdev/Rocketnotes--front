8affd00a382821e6fc47597cf247affbd907582a
Willian <willian.dev.ferreira@gmail.com>
2024-05-28 10:20:42 -0300

    refactor

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,9 +3,15 @@ import { Container, Profile, Logout } from "./styles";
 import { useAuth } from '../../hooks/auth';
 import { api } from '../../services/api';
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg"
+import { useNavigate } from 'react-router-dom';
 
 export function Header(){
     const {signOut, user} = useAuth();
+    const navigation = useNavigate();
+    function handleSignOut(){
+        navigation("/");
+        signOut();
+    }
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
@@ -19,7 +25,7 @@ export function Header(){
                 </div>
             </Profile>
 
-            <Logout onClick={signOut}>
+            <Logout onClick={handleSignOut}>
                 <RiShutDownLine/>
             </Logout>
         </Container>
